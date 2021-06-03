b937cc338a5856174181a97399821027381f2156
张超 <2458948167@qq.com>
2021-06-03 14:56:01 +0800

    1

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function getFileKey(file, prefix) {
   var str = file.path
     .replace(file.cwd, '')
     .replace(/\\/g, '/')
-    .replace(file.base, '')
+    .replace(file.base.replace(/\\/g, '/'), '')
     .replace(/^\/+/, '');
 
   return (
